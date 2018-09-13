f5b0c5ce1b644d7f2e4574e3e812a5788138d684
Rich Turnbull <rpturnbull1@gmail.com>
2018-09-13 14:25:54 -0700

    added Opera

---
+++ src/Integrations/Opera/SystemManager/index.js
@@ -0,0 +1,32 @@
+var Entity = require('../../../Entity');
+var Method = require('../../../Method');
+
+module.exports = new Entity('systemManagers', {
+  retrieve: new Method({
+    method: 'GET',
+    path: '/{id}',
+    urlParams: [ 'id' ]
+  }),
+  retrieveByHotel: new Method({
+    method: 'GET',
+    path: '/retrieveByHotel/{hotelId}',
+    urlParams: [ 'hotelId' ]
+  }),
+  search: new Method({
+    method: 'GET'
+  }),
+  setup: new Method({
+    method: 'POST',
+    path: '/setup'
+  }),
+  enable: new Method({
+    method: 'POST',
+    path: '/{id}/enable',
+    urlParams: [ 'id' ]
+  }),
+  disable: new Method({
+    method: 'POST',
+    path: '/{id}/disable',
+    urlParams: [ 'id' ]
+  }),
+});
