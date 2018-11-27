e4cc7ede7cd3c1a01b743d0c69e11d31c5615c89
Rich Turnbull <rpturnbull1@gmail.com>
2018-11-27 14:17:05 -0800

    refactored opera reports endpoint

--- src/Integrations/Opera/SystemManager/index.js
+++ src/Integrations/Opera/SystemManager/index.js
@@ -1,32 +1,37 @@
-var Entity = require('../../../Entity');
-var Method = require('../../../Method');
+var Entity = require('../../../Entity')
+var Method = require('../../../Method')
 
 module.exports = new Entity('systemManagers', {
   retrieve: new Method({
     method: 'GET',
     path: '/{id}',
-    urlParams: [ 'id' ]
+    urlParams: ['id'],
   }),
   retrieveByHotel: new Method({
     method: 'GET',
     path: '/retrieveByHotel/{hotelId}',
-    urlParams: [ 'hotelId' ]
+    urlParams: ['hotelId'],
   }),
   search: new Method({
-    method: 'GET'
+    method: 'GET',
   }),
   setup: new Method({
     method: 'POST',
-    path: '/setup'
+    path: '/setup',
   }),
   enable: new Method({
     method: 'POST',
     path: '/{id}/enable',
-    urlParams: [ 'id' ]
+    urlParams: ['id'],
   }),
   disable: new Method({
     method: 'POST',
     path: '/{id}/disable',
-    urlParams: [ 'id' ]
-  })
-});
+    urlParams: ['id'],
+  }),
+  fetchLatestReports: new Method({
+    method: 'GET',
+    path: '/{id}/fetchLatestReports',
+    urlParams: ['id'],
+  }),
+})
--- src/Integrations/Opera/index.js
+++ src/Integrations/Opera/index.js
@@ -1,8 +1,6 @@
 var Entity = require('../../Entity')
 var SystemManager = require('./SystemManager')
-var Report = require('./Report')
 
 module.exports = new Entity('OPERA', {
   SystemManager: SystemManager,
-  Report: Report,
 })
