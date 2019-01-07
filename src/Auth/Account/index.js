71435182fbee45e62e2fb9a98399ad9ba44dd093
Pete Turnbull <pete.j.turnbull@gmail.com>
2019-01-07 01:52:56 +0100

    fix retrieveByEmail

--- src/Auth/Account/index.js
+++ src/Auth/Account/index.js
@@ -1,43 +1,44 @@
-var Entity = require('../../Entity');
-var Method = require('../../Method');
-var Permission = require('./Permission');
+var Entity = require('../../Entity')
+var Method = require('../../Method')
+var Permission = require('./Permission')
 
 module.exports = new Entity('accounts', {
   Permission: Permission,
   retrieve: new Method({
     method: 'GET',
     path: '/{id}',
-    urlParams: [ 'id' ]
+    urlParams: ['id'],
   }),
   retrieveByEmail: new Method({
-    method: 'POST',
-    path: '/retrieveByEmail'
+    method: 'GET',
+    path: '/retrieveByEmail/{id}',
+    urlParams: ['id'],
   }),
   search: new Method({
-    method: 'GET'
+    method: 'GET',
   }),
   register: new Method({
     method: 'POST',
-    path: '/register'
+    path: '/register',
   }),
   setPassword: new Method({
     method: 'POST',
     path: '/{id}/setPassword',
-    urlParams: [ 'id' ]
+    urlParams: ['id'],
   }),
   activate: new Method({
     method: 'POST',
     path: '/{id}/activate',
-    urlParams: [ 'id' ]
+    urlParams: ['id'],
   }),
   deactivate: new Method({
     method: 'POST',
     path: '/{id}/deactivate',
-    urlParams: [ 'id' ]
+    urlParams: ['id'],
   }),
   endSessions: new Method({
     method: 'POST',
     path: '/{id}/endSessions',
-    urlParams: [ 'id' ]
-  })
-});
+    urlParams: ['id'],
+  }),
+})
