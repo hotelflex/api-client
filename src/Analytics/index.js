44bde8acd6f4313cdf49a3e62c0729276a9eb042
Rich Turnbull <rpturnbull1@gmail.com>
2019-01-30 16:33:54 -0800

    fix

--- src/Analytics/index.js
+++ src/Analytics/index.js
@@ -1,8 +1,6 @@
 var Entity = require('../Entity')
-var Report = require('./Report')
 var SystemManager = require('./SystemManager')
 
 module.exports = new Entity('ANYL', {
-  Report: Report,
   SystemManager: SystemManager,
 })
