fbd537d6eb575aa7dc195014199c960d0f024b12
Rich Turnbull <rpturnbull1@gmail.com>
2019-02-20 19:01:49 -0800

    invoice

---
+++ src/Accounting/Invoice/index.js
@@ -0,0 +1,22 @@
+var Entity = require('../../Entity')
+var Method = require('../../Method')
+
+module.exports = new Entity('invoices', {
+  retrieve: new Method({
+    method: 'GET',
+    path: '/{id}',
+    urlParams: ['id'],
+  }),
+  retrieveByNumber: new Method({
+    method: 'GET',
+    path: '/retrieveByNumber/{invoiceNumber}',
+    urlParams: ['invoiceNumber'],
+  }),
+  search: new Method({
+    method: 'GET',
+  }),
+  create: new Method({
+    method: 'POST',
+    path: '/create',
+  }),
+})
