88884bedbc674b9215b630c946d80f450296e39f
Rich Turnbull <rpturnbull1@gmail.com>
2019-03-27 10:18:39 -0700

    room night

--- src/InventoryManagement/index.js
+++ src/InventoryManagement/index.js
@@ -1,10 +1,12 @@
 var Entity = require('../Entity')
 var Block = require('./Block')
 var DaySlot = require('./DaySlot')
+var RoomNight = require('./RoomNight')
 var SystemManager = require('./SystemManager')
 
 module.exports = new Entity('INV', {
   Block: Block,
   DaySlot: DaySlot,
+  RoomNight: RoomNight,
   SystemManager: SystemManager,
 })
