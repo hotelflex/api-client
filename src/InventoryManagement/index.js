bb6bb5d5e922a433dda5834fd4597ce42663ff4a
Rich Turnbull <rpturnbull1@gmail.com>
2019-04-08 12:14:55 -0700

    removing DaySlot and Block

--- src/InventoryManagement/index.js
+++ src/InventoryManagement/index.js
@@ -1,12 +1,8 @@
 var Entity = require('../Entity')
-var Block = require('./Block')
-var DaySlot = require('./DaySlot')
 var RoomNight = require('./RoomNight')
 var SystemManager = require('./SystemManager')
 
 module.exports = new Entity('INV', {
-  Block: Block,
-  DaySlot: DaySlot,
   RoomNight: RoomNight,
   SystemManager: SystemManager,
 })
